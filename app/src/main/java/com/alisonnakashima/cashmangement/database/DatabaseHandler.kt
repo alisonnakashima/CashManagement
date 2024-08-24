c43d48a9003f9a67656dc1cc8993c3d0ded3582c
Alison Biazetto Nakashima <alisonbiazettonakashima@hotmail.com>
2024-08-24 10:17:53 -0300

    Commit - 24/08/2024

---
+++ app/src/main/java/com/alisonnakashima/cashmangement/database/DatabaseHandler.kt
@@ -0,0 +1,58 @@
+package com.alisonnakashima.cashmangement.database
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.database.Cursor
+
+
+import com.alisonnakashima.cashmangement.entity.Insertions
+
+
+class DatabaseHandler (context: Context): SQLiteOpenHelper (context, DATABASE_NAME, null, DATABABASE_VERSION ){
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL("CREATE TABLE IF NOT EXISTS ${TABLE_NAME} (_id INTEGER PRIMARY KEY AUTOINCREMENT, type TEXT, date DATE, description TEXT, value TEXT)")
+
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        db?.execSQL("DROP TABLE IF EXISTS ${TABLE_NAME}")
+        onCreate( db )
+    }
+
+    companion object {
+        private const val DATABASE_NAME = "db-insertions.sqlite"
+        private const val TABLE_NAME = "insertions"
+        private const val DATABABASE_VERSION = 1
+        private const val ID = 0
+        private const val TYPE = 1
+        private const val DATE = 2
+        private const val DESCRIPTION = 3
+        private const val VALUE = 4
+
+    }
+
+    fun insert(insertions: Insertions){
+        val db = this.writableDatabase
+
+        val registro = ContentValues()
+        registro.put("type", insertions.type)
+        registro.put("date", insertions.date)
+        registro.put("description", insertions.description)
+        registro.put("value", insertions.value)
+
+        db.insert(TABLE_NAME, null, registro)
+
+    }
+
+    fun entryList(){
+
+    }
+
+    fun balance(){
+
+    }
+
+
+}
---
+++ app/src/main/java/com/alisonnakashima/cashmangement/entity/Insertions.kt
@@ -0,0 +1,3 @@
+package com.alisonnakashima.cashmangement.entity
+
+class Insertions ( var _id: Int, var type: String, var date: String, var description: String, var value: String )
